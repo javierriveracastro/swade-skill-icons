d82475a703dfbd884df0a69a05d9d1387be89461
javier <javier@castroparga.com>
2021-01-11 18:02:00 +0100

    Innitial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+#SWADE Skill Icons
+
+This simple module add icons before skills for three SWADE Character sheets.
+
+## Official sheet
+
+
+![Offical Disabled](swade_skill_icons/docs/official_disable.png)
+
+![Offical Enabled](swade_skill_icons/docs/oficcial_enabled.png)
+
+## Comunity Sheet
+
+![Community Disabled](swade_skill_icons/docs/Comunity_disabled.png)
+
+![Community Enabled](swade_skill_icons/docs/community_enabled.png)
+
+## NPC Sheet
+
+![NPC Disabled](swade_skill_icons/docs/npc_disabled.png)
+
+![NPC Enabled](swade_skill_icons/docs/npc_enabled.png)
---
+++ swade_skill_icons/scripts/skillicons.js
@@ -0,0 +1,34 @@
+function add_icons (actor, html) {
+	// Remove all scrollables and inline actor styles
+	html.find('.scrollable').removeClass('scrollable');
+	html.find('.quickaccess-list, .inventory, .power-list, .skills-list, .gear-list, .gear.skills').css(
+		'overflow', 'visible');
+	let skill_list;
+	if (actor.data.type === "character") {
+		skill_list = html.find('li.item.skill');
+	} else {
+        skill_list = html.find('span.item.skill');
+	}
+	for (let skill_element of skill_list) {
+		let skill_wrapper = $(skill_element);
+		if (actor.data.type === 'npc') {
+			// Remove the block-inline style so the skills are shown one per
+			// line.
+			skill_wrapper.removeAttr("style");
+			skill_wrapper.attr('style', 'display:flex;');
+		}
+		let item_id = String(skill_wrapper.attr('data-item-id'));
+		let skill = actor.getOwnedItem(item_id);
+		skill_wrapper.prepend(`<img alt="roll" class="swade-skill-image" src="${skill.img}" data-item-id="${item_id}">`);
+	}
+}
+
+Hooks.on(`ready`, () => {
+	console.log('Skill Icons for SWADE | Ready');
+	const SHEETS = ['SwadeCharacterSheet', 'SwadeNPCSheet', 'CharacterSheet']
+    SHEETS.forEach(sheet_name => {
+        Hooks.on('render' +sheet_name, (app, html) => {
+            add_icons(app.object, html)
+        });
+    })
+});
